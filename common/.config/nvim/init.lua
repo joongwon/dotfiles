5af7a3fac35ebce30698dc44c1b2405e8024fd49
Joongwon Ahn <freleefty@gmail.com>
2026-05-15 20:27:52 +0900

    update nvim/init.lua

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -1,5 +1,5 @@
 -- dependencies: tree-sitter-cli, lua-language-server, ocaml-lsp-server, basedpyright, stylua, ruff, ocamlformat, zathura
-vim.pack.add({
+vim.pack.add {
   "https://github.com/github/copilot.vim",
   "https://github.com/ibhagwan/fzf-lua",
   "https://github.com/stevearc/oil.nvim",
@@ -9,34 +9,59 @@ vim.pack.add({
   "https://github.com/nvim-lualine/lualine.nvim",
   "https://github.com/hrsh7th/nvim-cmp",
   "https://github.com/hrsh7th/cmp-nvim-lsp",
-})
+  "https://github.com/akinsho/bufferline.nvim",
+}
 
-local cmp = require("cmp")
-cmp.setup({
+local cmp = require "cmp"
+cmp.setup {
   mapping = {
     ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+    ["<C-y>"] = cmp.mapping.confirm { select = true },
     ["<C-Space>"] = cmp.mapping.complete(),
   },
   sources = {
     { name = "nvim_lsp" },
   },
-})
+}
 
-require("lualine").setup({
+require("bufferline").setup {
+  options = {
+    diagnostics = "nvim_lsp",
+    always_show_bufferline = true,
+    show_buffer_icons = false,
+    show_buffer_close_icons = false,
+  },
+}
+
+require("lualine").setup {
   options = {
     theme = "auto",
     globalstatus = true,
   },
-})
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = {
+      "branch",
+      "diff",
+      {
+        "diagnostics",
+        symbols = {},
+      },
+    },
+    lualine_c = { "filename" },
+    lualine_x = { "encoding", "fileformat", "filetype" },
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
+  },
+}
 
-local conform = require("conform")
-conform.setup({
+local conform = require "conform"
+conform.setup {
   formatters_by_ft = {
     ["*"] = { "trim_whitespace" },
     lua = { "stylua" },
-    python = { "ruff" },
+    python = { "ruff_format" },
     ocaml = { "ocamlformat" },
   },
 
@@ -44,14 +69,19 @@ conform.setup({
     timeout_ms = 1000,
     lsp_fallback = true,
   },
-})
+}
 
-vim.g.vimtex_view_method = "zathura"
+for _, viewer in ipairs { "zathura", "skim" } do
+  if vim.fn.executable(viewer) == 1 then
+    vim.g.vimtex_view_method = viewer
+    break
+  end
+end
 
 vim.g.copilot_no_tab_map = true
 
-local ts = require("nvim-treesitter")
-ts.setup({})
+local ts = require "nvim-treesitter"
+ts.setup {}
 local ts_filetypes = {
   "python",
   "lua",
@@ -66,7 +96,7 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
-require("oil").setup({})
+require("oil").setup {}
 
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
@@ -110,24 +140,24 @@ for name, config in pairs(lspcfgs) do
   vim.lsp.enable(name)
 end
 
-local fzf = require("fzf-lua")
+local fzf = require "fzf-lua"
 
-vim.diagnostic.config({
+vim.diagnostic.config {
   virtual_text = true,
-})
+}
 
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostics" })
 vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "Find files" })
 vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "Live grep" })
 vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "List buffers" })
 vim.keymap.set("n", "<leader>fc", function()
-  fzf.files({ cwd = "~/.config", follow = true })
+  fzf.files { cwd = "~/.config", follow = true }
 end, { desc = "Find config files" })
 vim.keymap.set("n", "<leader>f", function()
-  conform.format({
+  conform.format {
     async = true,
     lsp_fallback = true,
-  })
+  }
 end, { desc = "Format file" })
 vim.keymap.set("i", "<C-M-;>", 'copilot#Accept("")', {
   expr = true,
@@ -140,3 +170,5 @@ vim.keymap.set("i", "<M-;>", "<plug>(copilot-accept-word)", {
 vim.keymap.set("i", "<C-;>", "<plug>(copilot-accept-line)", {
   desc = "Accept Copilot suggestion line by line",
 })
+vim.keymap.set("n", "gb", "<cmd>bnext<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "gB", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
