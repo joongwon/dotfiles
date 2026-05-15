1369b09c65a85fa74d458586ff1ab9eb274483e3
Joongwon Ahn <freleefty@gmail.com>
2026-05-15 20:26:08 +0900

    use nvim-cmp

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -1,13 +1,27 @@
+-- dependencies: tree-sitter-cli, lua-language-server, ocaml-lsp-server, basedpyright, stylua, ruff, ocamlformat, zathura
 vim.pack.add({
   "https://github.com/github/copilot.vim",
   "https://github.com/ibhagwan/fzf-lua",
-  "https://github.com/saghen/blink.lib",
-  "https://github.com/saghen/blink.cmp",
   "https://github.com/stevearc/oil.nvim",
   "https://github.com/nvim-treesitter/nvim-treesitter",
   "https://github.com/lervag/vimtex",
   "https://github.com/stevearc/conform.nvim",
   "https://github.com/nvim-lualine/lualine.nvim",
+  "https://github.com/hrsh7th/nvim-cmp",
+  "https://github.com/hrsh7th/cmp-nvim-lsp",
+})
+
+local cmp = require("cmp")
+cmp.setup({
+  mapping = {
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+  },
+  sources = {
+    { name = "nvim_lsp" },
+  },
 })
 
 require("lualine").setup({
@@ -31,29 +45,11 @@ conform.setup({
     lsp_fallback = true,
   },
 })
-vim.keymap.set("n", "<leader>f", function()
-  conform.format({
-    async = true,
-    lsp_fallback = true,
-  })
-end, { desc = "Format file" })
 
 vim.g.vimtex_view_method = "zathura"
 
-vim.keymap.set("i", "<C-M-;>", 'copilot#Accept("")', {
-  expr = true,
-  replace_keycodes = false,
-  desc = "Accept Copilot suggestion",
-})
-vim.keymap.set("i", "<M-;>", "<plug>(copilot-accept-word)", {
-  desc = "Accept Copilot suggestion word by word",
-})
-vim.keymap.set("i", "<C-;>", "<plug>(copilot-accept-line)", {
-  desc = "Accept Copilot suggestion line by line",
-})
 vim.g.copilot_no_tab_map = true
 
--- install: sudo pacman -S tree-sitter-cli
 local ts = require("nvim-treesitter")
 ts.setup({})
 local ts_filetypes = {
@@ -72,16 +68,6 @@ vim.api.nvim_create_autocmd("FileType", {
 
 require("oil").setup({})
 
-local cmp = require("blink.cmp")
---cmp.build():wait(60000)
-cmp.setup({
-  completion = {
-    documentation = {
-      auto_show = true,
-    },
-  },
-})
-
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
@@ -96,7 +82,6 @@ vim.opt.showbreak = "+++>"
 
 ---@type table<string, vim.lsp.Config>
 local lspcfgs = {
-  -- install: sudo pacman -S lua-language-server
   lua_ls = {
     cmd = { "lua-language-server" },
     filetypes = { "lua" },
@@ -109,13 +94,11 @@ local lspcfgs = {
       },
     },
   },
-  -- install: opam install ocaml-lsp-server
   ocamllsp = {
     cmd = { "opam", "exec", "--", "ocamllsp" },
     filetypes = { "ocaml" },
     root_markers = { { "dune-project" }, { "_opam" } },
   },
-  -- install: yay -S basedpyright
   basedpyright = {
     cmd = { "basedpyright-langserver", "--stdio" },
     filetypes = { "python" },
@@ -128,17 +111,32 @@ for name, config in pairs(lspcfgs) do
 end
 
 local fzf = require("fzf-lua")
-vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "Find files" })
-vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "Live grep" })
-vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "List buffers" })
-vim.keymap.set("n", "<leader>fc", function()
-  fzf.files({
-    cwd = "~/.config",
-    follow = true,
-  })
-end, { desc = "Find config files" })
 
 vim.diagnostic.config({
   virtual_text = true,
 })
+
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostics" })
+vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "Live grep" })
+vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "List buffers" })
+vim.keymap.set("n", "<leader>fc", function()
+  fzf.files({ cwd = "~/.config", follow = true })
+end, { desc = "Find config files" })
+vim.keymap.set("n", "<leader>f", function()
+  conform.format({
+    async = true,
+    lsp_fallback = true,
+  })
+end, { desc = "Format file" })
+vim.keymap.set("i", "<C-M-;>", 'copilot#Accept("")', {
+  expr = true,
+  replace_keycodes = false,
+  desc = "Accept Copilot suggestion",
+})
+vim.keymap.set("i", "<M-;>", "<plug>(copilot-accept-word)", {
+  desc = "Accept Copilot suggestion word by word",
+})
+vim.keymap.set("i", "<C-;>", "<plug>(copilot-accept-line)", {
+  desc = "Accept Copilot suggestion line by line",
+})
