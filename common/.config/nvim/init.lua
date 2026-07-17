074b2bbdca029340f1dd572b1066ab672231a1f0
Joongwon Ahn <freleefty@gmail.com>
2026-07-17 12:34:43 +0900

    Update from freleefty-home

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -324,8 +324,9 @@ vim.api.nvim_create_autocmd("FileType", {
 
 -- digraphs
 local digraphs = {
-  ["[["] = 0x27E6,
-  ["]]"] = 0x27E7,
+  ["[["] = 0x27E6, -- ⟦
+  ["]]"] = 0x27E7, -- ⟧
+  ["O+"] = 0x2295, -- ⊕
 }
 for k, v in pairs(digraphs) do
   vim.cmd(("digraph %s %d"):format(k, v))
