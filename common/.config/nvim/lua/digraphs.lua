39e3cb19d82fe3aa060dfc6ce7425cdb2e0962bd
Joongwon Ahn <freleefty@gmail.com>
2026-09-16 22:02:44 +0900

    update at home

--- common/.config/nvim/lua/digraphs.lua
+++ common/.config/nvim/lua/digraphs.lua
@@ -7,10 +7,11 @@ vim.fn.digraph_set("O+", "⊕")
 vim.fn.digraph_set("[[", "⟦")
 vim.fn.digraph_set("]]", "⟧")
 vim.fn.digraph_set("D=", "≜")
-vim.fn.digraph_set("h>", "↪") -- h for "hook", > for "right arrow"
-vim.fn.digraph_set("l>", "⇝") -- l for "leadsto", > for "right arrow"
 vim.fn.digraph_set("m>", "↦") -- m for "mapsto", > for "right arrow"
+vim.fn.digraph_set("h>", "↪")
+vim.fn.digraph_set("l>", "⇝")
 vim.fn.digraph_set("Ni", "∉") -- N for "not", i for "in"
+vim.fn.digraph_set("|.", "∣")
 
 vim.fn.digraph_set("-/", "∖")
 
